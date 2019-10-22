e2da3d05a91af2f896d66cd5982d90ed7dac0655
Yurchenko Denis <den4ik24@gmail.com>
2019-10-22 22:23:47 +0300

    обобщения2

---
+++ EXA/Интерфейсы.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace EXA
+{
+    public class Интерфейсы
+    {
+        static void Main()
+        {
+            var b = new Base();
+            b.Dispose();
+            ((IDisposable)b).Dispose();
+            Console.WriteLine("");
+
+            var d = new Derived();
+            d.Dispose();
+            ((IDisposable)d).Dispose();
+            Console.WriteLine();
+             b = new Derived();
+            b.Dispose();
+            ((IDisposable)b).Dispose();
+
+        }
+    }
+     public class Base : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Base's dispose"); 
+        }
+    }
+     public class Derived: Base, IDisposable
+    {
+        new public void Dispose()
+        {
+            Console.WriteLine("Derived's dispose");
+            //base.Dispose();
+        }
+    }
+
+}
--- EXA/обобщения.cs
+++ EXA/обобщения.cs
@@ -1,40 +1,103 @@
-﻿using System;
-namespace EXA
-{
-    public class обобщения
-    {
-        static void Main()
-        {
-            var bed = new IBed();
-            GenMethod<IBed> genMethod = new GenMethod<IBed>
-            {
-                Sleep = bed
-            };
+﻿////using System;
+////namespace gen
+////{
+////    class Program
+////    {
+////        static void Main()
+////        {
+////            Account acc1 = new Account(1) { Sum = 4500 };
+////            Account acc2 = new Account(2) { Sum = 5000 };
+////            Transaction<Account> transaction = new Transaction<Account>
+////            {
+////                FromAccount = acc1,
+////                ToAccount = acc2,
+////                Sum = 6900
+////            };
+////            transaction.Execute();
 
-            genMethod.Go<string>(bed); 
+////        }
+////    }
+
+////    class Account
+////    {
+////        public int Id { get; private set; }
+////        public int Sum { get; set; }
+////        public Account(int _id)
+////        {
+////            Id = _id;
+////        }
+////    }
+
+////    class Transaction<T> where T : Account
+////    {
+////        public T FromAccount { get; set; }
+////        public T ToAccount { get; set; }
+////        public int Sum { get; set; }
+
+////        public void Execute()
+////        {
+////            if (FromAccount.Sum > Sum)
+////            {
+////                FromAccount.Sum -= Sum;
+////                ToAccount.Sum += Sum;
+////                Console.WriteLine();
+
+////                Console.WriteLine($"{FromAccount.Id}:{FromAccount.Sum} - {ToAccount.Id}:{ToAccount.Sum}");
+////            }
+////            else
+////            {
+////                Console.WriteLine($"нет денег на {FromAccount.Id}");
+////            }
+////        }
+////    }
+////}
+
+//using System;
+//namespace EXA
+//{
+//    public class обобщения
+//    {
+//        static void Main()
+//        {
+//            //var bed = new IBed();
+//            var bed = new Bed();
+//            GenMethod<IBed> genMethod = new GenMethod<IBed>
+//            {
+//               //Sleep = genMethod
+//            };
             
-        }
-    }
+//            genMethod.Go(bed);
+            
+//        }
+//    }
 
-    class IBed
-    {
-        public void TimeToSleep()
-        {
-            Console.WriteLine("Спатки пора");
-        }
-    }
+//    class Bed : IBed
+//    {
+        
+//        public void TimeToSleep()
+//        {
+//            Console.WriteLine("Спатки пора");
+//        }
+//    }
 
-    class GenMethod<T> where T : IBed
-    {
-        public T Sleep;
-        public T1 Go<T1>(T1 a)
-        {
-            return a;
-        }
-        public void Go<T1>(IBed b)
-        {
+//    public interface IBed
+//    {
+//        void TimeToSleep();
+       
+//    }
 
-            Console.WriteLine("Спи давай");
-        }
-    }
-}
+//    class GenMethod<T> where T : IBed
+//    {
+       
+//        //public T Sleep;
+//        //public T1 Go<T1>(T1 a)
+//        //{
+//        //    return a;
+//        //}
+//        public void Go(T b)
+//        {
+//            Console.WriteLine("Спи давай");
+//            //Sleep.TimeToSleep();
+//        }
+//    }
+//}
