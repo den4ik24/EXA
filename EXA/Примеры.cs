36c169f4e1baef1ccbdba886f8c65397020c92a7
Yurchenko Denis <den4ik24@gmail.com>
2019-10-22 17:43:51 +0300

    обобщения

--- EXA/Примеры.cs
+++ EXA/Примеры.cs
@@ -1,124 +1,149 @@
-﻿//using System;
+﻿////using System;
 
-//namespace ConsoleApplication1
-//{
-//    class Program
-//    {
-//        static void Main()
-//        {
-//            Counter c = new Counter(new Random().Next(10));
-//            c.ThresholdReached += c_ThresholdReached;
-
-//            Console.WriteLine("press 'a' key to increase total");
-//            while (Console.ReadKey(true).KeyChar == 'a')
-//            {
-//                Console.WriteLine("adding one");
-//                c.Add(1);
-//            }
-//        }
-
-//        static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
-//        {
-//            Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
-//            Environment.Exit(0);
-//        }
-//    }
-
-//    class Counter
-//    {
-//        private int threshold;
-//        private int total;
-
-//        public Counter(int passedThreshold)
-//        {
-//            threshold = passedThreshold;
-//        }
-
-//        public void Add(int x)
-//        {
-//            total += x;
-//            if (total >= threshold)
-//            {
-//                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
-//                args.Threshold = threshold;
-//                args.TimeReached = DateTime.Now;
-//                OnThresholdReached(args);
-//            }
-//        }
-
-//        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
-//        {
-//            ThresholdReached?.Invoke(this, e);
-//        }
-
-//        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
-//    }
-
-//    public class ThresholdReachedEventArgs : EventArgs
-//    {
-//        public int Threshold { get; set; }
-//        public DateTime TimeReached { get; set; }
-//    }
-//}
-
-////using System;
-////namespace EXA
+////namespace ConsoleApplication1
 ////{
-////    public class Примеры
+////    class Program
 ////    {
 ////        static void Main()
 ////        {
-////            Param(z: 3, x: 4, y: 5);
-////            var o = new Object();
+////            Counter c = new Counter(new Random().Next(10));
+////            c.ThresholdReached += c_ThresholdReached;
 
-////            int P;
-////            int x = 2;
-////            int y = 3;
-////            Ref(ref x, y, ref o);
-
-////            Out(out x, y);
-
-////            Person person = new Person() { Name = "AA", Surname = "BB" };
-
-////           Person [] array = {
-////                new Person { Name = "1" },
-////                new Person { Name = "2" },
-////                new Person { Name = "3" }
-////            };
-////            //анонимный тип
-////            var Pofig = new {Name = "Pofig_Name", Surname = "Pofig_Surname" };
-////            Console.WriteLine(Pofig.Surname);
-////            dynamic user = new { Name = "Tom ", Age = 34 };
-////            MyMethod(user);
+////            Console.WriteLine("press 'a' key to increase total");
+////            while (Console.ReadKey(true).KeyChar == 'a')
+////            {
+////                Console.WriteLine("adding one");
+////                c.Add(1);
+////            }
 ////        }
 
-////        public static int Param(int x, int y, int z = 2)
+////        static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
 ////        {
-////            return (x + y) * z;
-////        }
-
-////        static void Ref(ref int x, int y, ref object o)
-////        {
-////            var p = new object();
-////            x = x * y;
-////            o = p;
-////        }
-
-////        static void Out(out int x, int y)
-////        {
-////            x = 3;
-////            y += x;
-////        }
-////        public static void MyMethod(dynamic parameter)
-////        {
-////            Console.WriteLine(parameter.Name + "" + parameter.Age);
+////            Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
+////            Environment.Exit(0);
 ////        }
 ////    }
 
-////    class Person
+////    class Counter
 ////    {
-////        public string Name { get; set; }
-////        public string Surname { get; set; }
+////        private int threshold;
+////        private int total;
+
+////        public Counter(int passedThreshold)
+////        {
+////            threshold = passedThreshold;
+////        }
+
+////        public void Add(int x)
+////        {
+////            total += x;
+////            if (total >= threshold)
+////            {
+////                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+////                args.Threshold = threshold;
+////                args.TimeReached = DateTime.Now;
+////                OnThresholdReached(args);
+////            }
+////        }
+
+////        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+////        {
+////            ThresholdReached?.Invoke(this, e);
+////        }
+
+////        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
 ////    }
 
+////    public class ThresholdReachedEventArgs : EventArgs
+////    {
+////        public int Threshold { get; set; }
+////        public DateTime TimeReached { get; set; }
+////    }
 ////}
+
+//////using System;
+//////namespace EXA
+//////{
+//////    public class Примеры
+//////    {
+//////        static void Main()
+//////        {
+//////            Param(z: 3, x: 4, y: 5);
+//////            var o = new Object();
+
+//////            int P;
+//////            int x = 2;
+//////            int y = 3;
+//////            Ref(ref x, y, ref o);
+
+//////            Out(out x, y);
+
+//////            Person person = new Person() { Name = "AA", Surname = "BB" };
+
+//////           Person [] array = {
+//////                new Person { Name = "1" },
+//////                new Person { Name = "2" },
+//////                new Person { Name = "3" }
+//////            };
+//////            //анонимный тип
+//////            var Pofig = new {Name = "Pofig_Name", Surname = "Pofig_Surname" };
+//////            Console.WriteLine(Pofig.Surname);
+//////            dynamic user = new { Name = "Tom ", Age = 34 };
+//////            MyMethod(user);
+//////        }
+
+//////        public static int Param(int x, int y, int z = 2)
+//////        {
+//////            return (x + y) * z;
+//////        }
+
+//////        static void Ref(ref int x, int y, ref object o)
+//////        {
+//////            var p = new object();
+//////            x = x * y;
+//////            o = p;
+//////        }
+
+//////        static void Out(out int x, int y)
+//////        {
+//////            x = 3;
+//////            y += x;
+//////        }
+//////        public static void MyMethod(dynamic parameter)
+//////        {
+//////            Console.WriteLine(parameter.Name + "" + parameter.Age);
+//////        }
+//////    }
+
+//////    class Person
+//////    {
+//////        public string Name { get; set; }
+//////        public string Surname { get; set; }
+//////    }
+
+//////}
+//using System;
+
+//class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        int x = 7;
+//        int y = 25;
+//        Swap<int>(ref x, ref y); // или так Swap(ref x, ref y);
+//        Console.WriteLine($"x={x}    y={y}");   // x=25   y=7
+
+//        string s1 = "hello";
+//        string s2 = "bye";
+//        Swap<string>(ref s1, ref s2); // или так Swap(ref s1, ref s2);
+//        Console.WriteLine($"s1={s1}    s2={s2}"); // s1=bye   s2=hello
+
+//        Console.Read();
+//    }
+//    public static void Swap<T>(ref T x, ref T y)
+//    {
+//        T temp = x;
+//        x = y;
+//        y = temp;
+//    }
+//}
--- EXA/обобщения.cs
+++ EXA/обобщения.cs
@@ -1,11 +1,40 @@
-﻿//using System;
-//namespace EXA
-//{
-//    public class обобщения
-//    {
-//        static void Main()
-//        {
+﻿using System;
+namespace EXA
+{
+    public class обобщения
+    {
+        static void Main()
+        {
+            var bed = new IBed();
+            GenMethod<IBed> genMethod = new GenMethod<IBed>
+            {
+                Sleep = bed
+            };
 
-//        }
-//    }
-//}
+            genMethod.Go<string>(bed); 
+            
+        }
+    }
+
+    class IBed
+    {
+        public void TimeToSleep()
+        {
+            Console.WriteLine("Спатки пора");
+        }
+    }
+
+    class GenMethod<T> where T : IBed
+    {
+        public T Sleep;
+        public T1 Go<T1>(T1 a)
+        {
+            return a;
+        }
+        public void Go<T1>(IBed b)
+        {
+
+            Console.WriteLine("Спи давай");
+        }
+    }
+}
