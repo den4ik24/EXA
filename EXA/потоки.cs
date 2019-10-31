a969ac2d8b106e1ce33d09ea4d5e03f927372085
Yurchenko Denis <den4ik24@gmail.com>
2019-10-31 11:59:29 +0300

    потоки

---
+++ EXA/потоки.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace EXA
+{
+    public class потоки
+    {
+        public static void Main()
+        {
+            Thread t = new Thread(Worker);
+            //t.IsBackground = true;
+            t.Start();
+            Console.WriteLine("Возврат из Main");
+        }
+
+        private static void Worker()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Возврат из Worker");
+        }
+    }
+}
--- EXA/сериализация.cs
+++ EXA/сериализация.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Runtime.Serialization.Formatters.Binary;
 
-namespace EXA
-{
-    public class сериализация
-    {
-        public static void Main()
-        {
-            //создание графа объектов для последующей сериализации в поток
-            var objectGraph = new List<string> {"AAA", "BBB", "CCC", "DDD"};
-            Stream stream = SerializableToMemory(objectGraph);
+//namespace EXA
+//{
+//    public class Сериализация
+//    {
+//        public static void Main()
+//        {
+//            //создание графа объектов для последующей сериализации в поток
+//            var objectGraph = new List<string> {"AAA", "BBB", "CCC", "DDD"};
+//            Stream stream = SerializableToMemory(objectGraph);
 
-            //Обнуляем все для данного примера
-            stream.Position = 0;
-            objectGraph = null;
+//            //Обнуляем все для данного примера
+//            stream.Position = 0;
+//            //objectGraph = null;
 
-            //Десериализация объектов и проверка их работоспособности
-            objectGraph = (List<string>)
-                DeserializeFromMemory(stream);
-            foreach (var s in objectGraph)
-                Console.WriteLine(s);
-        }
+//            //Десериализация объектов и проверка их работоспособности
+//            objectGraph = (List<string>)
+//                DeserializeFromMemory(stream);
+//            foreach (var s in objectGraph)
+//                Console.WriteLine(s);
+//        }
 
-        private static MemoryStream SerializableToMemory (object objectGraph)
-        {
+//        private static MemoryStream SerializableToMemory (object objectGraph)
+//        {
 
-     //Конструирование потока, который будет содержать сериализованные объекты
-            MemoryStream stream = new MemoryStream();
+//     //Конструирование потока, который будет содержать сериализованные объекты
+//            MemoryStream stream = new MemoryStream();
 
-            //Задание форматирования при сериализации
-            BinaryFormatter formatter = new BinaryFormatter();
+//            //Задание форматирования при сериализации
+//            BinaryFormatter formatter = new BinaryFormatter();
 
-            //Заставляем модуль форматирования сериализовать объекты в поток
-            formatter.Serialize(stream, objectGraph);
+//            //Заставляем модуль форматирования сериализовать объекты в поток
+//            formatter.Serialize(stream, objectGraph);
 
-            //Возвращение потока сериализованных объектов вызывающему методу
-            return stream;
-        }
+//            //Возвращение потока сериализованных объектов вызывающему методу
+//            return stream;
+//        }
 
-        private static object DeserializeFromMemory(Stream stream)
-        {
-            //Задание форматирования при сериализации
-            BinaryFormatter formatter = new BinaryFormatter();
+//        private static object DeserializeFromMemory(Stream stream)
+//        {
+//            //Задание форматирования при сериализации
+//            BinaryFormatter formatter = new BinaryFormatter();
 
-            //Заставляем модуль форматирования десериализовать объекты из потока
-            return formatter.Deserialize(stream);
-        }
-    }
-}
+//            //Заставляем модуль форматирования десериализовать объекты из потока
+//            return formatter.Deserialize(stream);
+//        }
+//    }
+//}
